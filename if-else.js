f7884a592e134602674355caa6795ccb6f794d74
KapseRohit <kapserohit77@gmail.com>
2023-05-27 21:34:00 +0530

    practice

---
+++ if-else.js
@@ -0,0 +1,41 @@
+// var num1= 27;
+// var num2= 123;
+// var addition= num1+num2;
+// console.log(addition)
+
+// var count=50;
+// var number=40;
+// var substraction= number-count;
+// console.log(substraction)
+
+// var number1=25;
+// var number2=2;
+// var multiplication=number1*number2;
+// console.log(multiplication)
+
+// var number1=35;
+// var number2=5;
+// var divide=number1/number2;
+// console.log(divide)
+
+// var i=5;
+// console.log(i)
+// i=i+5;
+// i++;
+// i++;
+// i++;
+// // ++ variable ads one in each example
+// console.log(i)
+
+// var r=10;
+// console.log(r)
+// r= r*10;
+// r--;
+// r--;
+// console.log(r)
+
+// conditional Statements
+// 1. if else
+// 2. switch
+
+
